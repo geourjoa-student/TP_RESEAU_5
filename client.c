fbb111ade785d92a959c7fc84d1e760a87ef5733
Anthony Geourjon <anthony@geourjoa-desktop>
2016-04-28 22:14:32 +0200

    Formatage de code .gitignore

--- client.c
+++ client.c
@@ -1,22 +1,9 @@
-/******************************************************************************/
-/*			Application: ...					*/
-/******************************************************************************/
-/*									      */
-/*			 programme  CLIENT				      */
-/*									      */
-/******************************************************************************/
-/*									      */
-/*		Auteurs : ... 					*/
-/*									      */
-/******************************************************************************/	
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <curses.h> 		/* Primitives de gestion d'Žcran */
+#include <curses.h> 		
 #include <sys/signal.h>
 #include <sys/wait.h>
-#include "fon.h"   		/* primitives de la boite a outils */
+#include "fon.h"   		
 
 #define SERVICE_DEFAUT "1111"
 #define PROTOCOLE_DEFAUT "udp"
@@ -31,29 +18,29 @@ void client_appli (char *serveur, char *service, char *protocole);
 main(int argc, char *argv[])
 {
 
-	char *serveur= SERVEUR_DEFAUT; /* serveur par defaut */
-	char *service= SERVICE_DEFAUT; /* numero de service par defaut (no de port) */
-	char *protocole= PROTOCOLE_DEFAUT; /* protocole par defaut */
+	char *serveur= SERVEUR_DEFAUT;
+	char *service= SERVICE_DEFAUT; /* no de port) */
+	char *protocole= PROTOCOLE_DEFAUT; 
 
 	/* Permet de passer un nombre de parametre variable a l'executable */
 	switch(argc)
 	{
-		case 1 :		/* arguments par defaut */
+		case 1 :		
 			  printf("serveur par defaut: %s\n",serveur);
 			  printf("service par defaut: %s\n",service);
 			  printf("protocole par defaut: %s\n",protocole);
 			  break;
-		case 2 :		/* serveur renseigne  */
+		case 2 :		
 			  serveur=argv[1];
 			  printf("service par defaut: %s\n",service);
 			  printf("protocole par defaut: %s\n",protocole);
 			  break;
-		case 3 :		/* serveur, service renseignes */
+		case 3 :		
 			  serveur=argv[1];
 			  service=argv[2];
 			  printf("protocole par defaut: %s\n",protocole);
 			  break;
-		case 4:			/* serveur, service, protocole renseignes */
+		case 4:		
 			  serveur=argv[1];
 			  service=argv[2];
 			  protocole=argv[3];
@@ -69,6 +56,8 @@ main(int argc, char *argv[])
 	/* protocole le protocole qui sera utilise pour la communication */
 	
 	client_appli(serveur,service,protocole);
+
+	return EXIT_SUCCESS;
 }
 void viderbuffer()
 {
@@ -84,7 +73,7 @@ void client_appli (char *serveur,char *service,char *protocole)
 	
   	char c[10]; // test de chiasse
   	
-	int taille_mot; // taille du mot a trouvé
+	int taille_mot; 
 	
 	int jeu_fini=0; // indicateur de jeu fini
 	
@@ -93,9 +82,10 @@ void client_appli (char *serveur,char *service,char *protocole)
 	struct sockaddr_in p_adr_distant;
 	
 	//Création socket
-	int socket_local = h_socket(AF_INET,SOCK_STREAM); // mettre AF_INET apres
+	int socket_local = h_socket(AF_INET,SOCK_STREAM); 
 	
-	adr_socket(service,serveur,protocole,&p_adr_distant);// initialise p_adr_distant
+	//Initialisation du socket
+	adr_socket(service,serveur,protocole,&p_adr_distant);
 	
 	//Connexion au serveur
 	h_connect(socket_local,&p_adr_distant);
@@ -105,28 +95,37 @@ void client_appli (char *serveur,char *service,char *protocole)
 	//taille_mot= c[0] -'0';
 	taille_mot=5;
 	printf("taille mot : %d \n",taille_mot);
+	
 	char mot_trouve[taille_mot]; // declaration du tableau du mot_courant
+	
 	printf("\nBienvenue dans le jeu du pendu !\n");
+
+	printf("Voici le mot actuel : \n");
+	for(i=0; i<taille_mot; i++){
+		printf(".");
+	}
+
+	printf("\n");
+
 	while(!jeu_fini)
 	{
-		printf("Donnez une lettre \n");
+		printf("Saisissez une lettre : ");
 		scanf("%c",&c[0]);// lit le char
+		
 		viderbuffer();
-		printf("c[0] : %c",c[0]);
 		
-		h_writes(socket_local,&c[0],sizeof(char)); // envoie le char au serveur
-		h_reads(socket_local,mot_trouve,taille_mot); // lit l'état du mot
-		h_reads(socket_local,Tampon,sizeof(char)); // recupere le nombre d'erreur
+		h_writes(socket_local,&c[0],sizeof(char)); // Ecriture de la lettre joué
+		h_reads(socket_local,mot_trouve,taille_mot); // Lecture de l'état du mot
 		
-		
-		//nb_erreurs= Tampon[0] -'0';
+		h_reads(socket_local,Tampon,sizeof(char)); // Lecture du nombre d'erreur		
 		nb_erreurs= Tampon[0];
 		
 		printf("Voici le mot actuel : \n");
 		for(i=0;i<taille_mot;i++)
-		printf("%c",mot_trouve[i]);
+			printf("%c",mot_trouve[i]);
 		printf("\n Vous avez encore droit a %d erreurs\n",nb_erreurs);
-		h_reads(socket_local,Tampon,sizeof(char)); // recupere le boolean de fin de partie
+		
+		h_reads(socket_local,Tampon,sizeof(char)); // rLecture du bouléen d'état fin de partie
 		jeu_fini=Tampon[0];
 	}
 	
